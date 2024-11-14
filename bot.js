0a6b93d204377cc8a09c99a36a1e8772799ea97e
Shadhin <01shadhin33@gmail.com>
2024-11-14 12:11:12 +0600

    integration listening mode

--- bot.js
+++ bot.js
@@ -1,19 +1,45 @@
-const { ActivityHandler, MessageFactory } = require('botbuilder');
+const { ActivityHandler, MessageFactory, ActivityTypes } = require('botbuilder');
 
 class EchoBot extends ActivityHandler {
-    constructor() {
+    constructor(conversationReferences) {
         super();
+
+        this.conversationReferences = conversationReferences;
+
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            const replyText = `Echo: ${ context.activity.text }`;
-            await context.sendActivity(MessageFactory.text(replyText, replyText));
+
+            if (context.activity.conversation.id === process.env.DEV_ID) {
+                let msg = context.activity.text;
+                if(this.startsWithAll(msg)){
+                    msg = this.removeAtAll(msg);
+
+                    for (const conversationReference of Object.values(this.conversationReferences)) {
+                        await context.adapter.continueConversationAsync(process.env.MicrosoftAppId, conversationReference, async context => {
+                            await context.sendActivity(MessageFactory.text(msg, msg));
+                        });
+                    }
+                }
+                else{
+                    await context.sendActivity(MessageFactory.text(msg, msg));
+                }
+            }
+            else {
+                await context.sendActivities([
+                    { type: ActivityTypes.Typing },
+                    { type: 'delay', value: 1000 }
+                ]);
+                const replyText = `You said: ${context.activity.text}. Why?`;
+                await context.sendActivity(MessageFactory.text(replyText, replyText));
+            }
+
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
 
         this.onMembersAdded(async (context, next) => {
             const membersAdded = context.activity.membersAdded;
-            const welcomeText = 'Hello and welcome!';
+            const welcomeText = "Welcome to PingPal!\n\nIt keeps the Mobile Apps Team on track with quick reminders and updates!\n\nCurrently, PingPal is designed only to notify about the standup sheet and doesn't respond to messages. Future updates will introduce new features, including the ability to respond to your replies.\n\nStay tuned for more!";
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
                     await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
@@ -23,6 +49,15 @@ class EchoBot extends ActivityHandler {
             await next();
         });
     }
+
+    removeAtAll(sentence) {
+        return sentence.replace(/^@all\s+/, '');
+    }
+
+    startsWithAll(sentence) {
+        return sentence.startsWith('@all');
+    }
+
 }
 
 module.exports.EchoBot = EchoBot;
--- index.js
+++ index.js
@@ -18,14 +18,31 @@ const {
 // This bot's main dialog.
 const { EchoBot } = require('./bot');
 
+const conversationReferences = {};
+
+const users = JSON.parse(process.env.USERS);
+
+const conversationList = users.map(({ id, name }) => ({
+    conversation: { id: id },
+    channelId: "skype",
+    locale: "en-US",
+    serviceUrl: "https://smba.trafficmanager.net/apis/"
+}));
+
+conversationList.forEach(conversationReference => {
+    const conversationId = conversationReference.conversation.id;
+    conversationReferences[conversationId] = conversationReference;
+});
+
 // Create HTTP server
 const server = restify.createServer();
 server.use(restify.plugins.bodyParser());
 
 server.listen(process.env.port || process.env.PORT || 3978, () => {
-    console.log(`\n${ server.name } listening to ${ server.url }`);
+    console.log(`\n${server.name} listening to ${server.url}`);
     console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
     console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
+
 });
 
 const credentialsFactory = new ConfigurationServiceClientCredentialFactory({
@@ -40,35 +57,36 @@ const botFrameworkAuthentication = createBotFrameworkAuthenticationFromConfigura
 // Create adapter.
 // See https://aka.ms/about-bot-adapter to learn more about adapters.
 const adapter = new CloudAdapter(botFrameworkAuthentication);
+// adapter.use(new ShowTypingMiddleware());
 
 // Catch-all for errors.
 const onTurnErrorHandler = async (context, error) => {
     // This check writes out errors to console log .vs. app insights.
     // NOTE: In production environment, you should consider logging this to Azure
     //       application insights.
-    console.error(`\n [onTurnError] unhandled error: ${ error }`);
+    console.error(`\n [onTurnError] unhandled error: ${error}`);
 
     // Send a trace activity, which will be displayed in Bot Framework Emulator
     await context.sendTraceActivity(
         'OnTurnError Trace',
-        `${ error }`,
+        `${error}`,
         'https://www.botframework.com/schemas/error',
         'TurnError'
     );
 
     // Send a message to the user
-    await context.sendActivity('The bot encountered an error or bug.');
-    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
+    // await context.sendActivity('The bot encountered an error or bug.');
+    // await context.sendActivity('To continue to run this bot, please fix the bot source code.');
 };
 
 // Set the onTurnError for the singleton CloudAdapter.
 adapter.onTurnError = onTurnErrorHandler;
 
 // Create the main dialog.
-const myBot = new EchoBot();
+const myBot = new EchoBot(conversationReferences);
 
 // Listen for incoming requests.
-server.post('/api/messages', async (req, res) => {
+server.post('/pingpal/api/messages', async (req, res) => {
     // Route received a request to adapter for processing
     await adapter.process(req, res, (context) => myBot.run(context));
 });
